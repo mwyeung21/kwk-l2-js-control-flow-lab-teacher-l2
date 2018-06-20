02ab42b6c872d3f03501232223579fba6afd6f35
Melany Yeung <mwyeung21@gmail.com>
2018-06-20 16:06:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function scuberGreetingForFeet(input)
   if (input <= 400)
     return 'This one is on me!'
   else if (input > 2000)
-    return 'I will gladly take your '
+    return 'I will gladly take your thirty bucks.'
 }
